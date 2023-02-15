dc7a9bec40ef001e80fd8b5a6f9a73eb3fadf07d
Mewerton Melo <mewerton@gmail.com>
2023-02-15 14:58:32 -0300

    Update index.jsx

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,4 +1,3 @@
-import { Footer } from "../../components/Footer";
 import { Header } from "../../components/Header";
 import { Hero } from "../../components/Hero";
 import { Container } from "./styles";
