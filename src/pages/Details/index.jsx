b2e7a044cbe1fda37bd8fea0c384120229e9f209
Mewerton Melo <mewerton@gmail.com>
2023-02-24 14:45:31 -0300

    Update index.jsx

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,6 +1,6 @@
 // import { Header } from "../../components/Header"
 import { Container } from "./styles"
-import {BrowserRouter, Routes, Route, useLocation } from "react-router-dom"
+import {BrowserRouter, Routes, Route, useLocation, Navigate } from "react-router-dom"
 import { Home } from "../Home"
 import { Sobre } from "../Sobre"
 import { Cursos } from "../Cursos"
@@ -27,6 +27,7 @@ export function Details(){
             <Route path="/sobre" element={<Sobre/>}/>
             <Route path="/terapeuras" element={<Terapeutas/>}/>
             <Route path="/contato" element={<Contato/>}/>
+            <Route path="*" element={<Navigate to="/"/>}/>
           </Routes>
         </AnimatePresence>
         
