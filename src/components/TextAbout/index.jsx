23411ef24ee9f7f2562dbcf1f7be99ef46116246
Mewerton Melo <mewerton@gmail.com>
2023-06-18 19:07:34 -0300

    Alterações no SOBRE

--- src/components/TextAbout/index.jsx
+++ src/components/TextAbout/index.jsx
@@ -10,22 +10,24 @@ export function TextAbout(){
             <div className="text">
                 <br />
                 <p>
-                Somos apaixonadas pela Terapia do Esquema, nos conhecemos no curso de formação, nos tornamos amigas e adorávamos nos reunir para discutir nossas descobertas e ideias, sempre acompanhadas de uma taça de vinho.
+                Somos três psicólogas apaixonadas pela Terapia do Esquema. Nos conhecemos em 2018 durante a formação em terapia do esquema, nos tornamos amigas e começamos a nos reunir para compartilhar nossas descobertas, discutir casos e estudar de forma mais profunda a abordagem. Sempre acompanhadas de um bom cafezinho ou uma taça de vinho.
                 <br />
                 <br />
 
-                Mas, além do nosso amor pela  Terapia do Esquema, também tínhamos o desejo de compartilhar nossos conhecimentos com um público maior, percebemos que havia uma demanda grande por conhecimento sobre a Terapia do Esquema e decidimos unir suas forças para compartilhar nossos conhecimentos com mais pessoas. Assim nasceu o projeto “Esquematiza Psicologia”.
+                Mas, além do nosso amor pela Terapia do Esquema, também tínhamos o desejo de compartilhar nossos conhecimentos com um público maior e formar uma rede de apoio para outros psis. E assim nasceu o projeto “Esquematiza Psicologia”!
+
                 <br />
                 <br />
 
-                O objetivo era criar um curso online para psicólogos que desejavam entender mais sobre a Terapia do Esquema e poder aplicá-la em seus pacientes de forma eficaz. Assim compartilhamos nossos conhecimentos e habilidades para criar um curso atualizado, que cobre os principais aspectos da terapia do esquema.
+                O objetivo era criar um grupo de estudos online para psicólogos que desejavam entender mais sobre a Terapia do Esquema e poder aplicá-la em seus pacientes de forma eficaz. Assim criamos um grupo atualizado, que cobre os principais aspectos da terapia do esquema.
                 <br />
                 <br />
 
-                A criação do curso foi um desafio e tanto, estávamos determinadas a oferecer o melhor conteúdo possível. Dividimos tarefas, cada uma trazendo sua expertise e experiência para a equipe. A colaboração mútua foi fundamental para o sucesso do projeto.
+                Foi um desafio e tanto, estávamos determinadas a oferecer o melhor conteúdo possível. Dividimos tarefas, cada uma trazendo sua sabedoria e experiência para a equipe. A colaboração mútua foi fundamental para o sucesso do projeto.
+
                 <br />
                 <br />
-                O primeiro grupo de estudos foi lançado em 2020, muitos psicólogos curiosos sobre a Terapia do Esquema se inscreveram, o que nos motivou bastante a continuar esse projeto que temos tanto carinho que que gaças a Deus vem crescendo mais a cada dia.
+                O primeiro grupo de estudos foi lançado em 2021, muitos psicólogos curiosos sobre a Terapia do Esquema e outros que já eram terapeutas do esquema se inscreveram, o que nos motivou bastante a continuar esse projeto que temos tanto carinho e que vem crescendo mais a cada dia.
                 <br />
                 <br />
                 Em resumo, a história do “Esquematiza Psicologia” é um exemplo de como a paixão e a amizade podem ser combinadas para criar algo realmente transformador. Nosso projeto não apenas ensina a Terapia do Esquema, mas também inspira outros a seguirem seus sonhos e a se unirem em causas que acreditam.
